7500c46da083e12209b6da614df824b8b37d7fcb
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-10-14 17:41:36 +0900

    Update BI_CSYSCD.sql

--- BI_CSYSCD.sql
+++ BI_CSYSCD.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: my-mariadb:3306
--- Generation Time: May 21, 2024 at 07:37 AM
--- Server version: 11.3.2-MariaDB
--- PHP Version: 8.2.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `baseinfo`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `BI_CSYSCD`
---
-
 CREATE TABLE `BI_CSYSCD` (
   `GRP_ID` varchar(4) NOT NULL,
   `CD` varchar(7) NOT NULL,
